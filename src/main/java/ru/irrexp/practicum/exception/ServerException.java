efefd8319386ad49eeb777fb67b7612972ca95ef
avinokurov <avinokurov@fil-it.ru>
2026-07-06 00:03:49 +0300

    Зависимости и настройки

---
+++ src/main/java/ru/irrexp/practicum/exception/ServerException.java
@@ -0,0 +1,9 @@
+package ru.irrexp.practicum.exception;
+
+public class ServerException extends RuntimeException {
+
+    public ServerException(String message) {
+        super(message);
+    }
+
+}
